716a1ba7490e3aaaf42e66728c75537332cefe10
yeshan.ye <yeshan.ye@alibaba-inc.com>
2024-04-11 00:53:08 +0800

    fix: vfox search

--- lib/mongo_utils.lua
+++ lib/mongo_utils.lua
@@ -3,7 +3,7 @@ local http = require("http")
 local mongo_utils = {}
 
 function mongo_utils.get_mongo_release_verions()
-    local fetch_url = "https://fastly.jsdelivr.net/gh/yeshan333/vfox-mongo@main/assets/" .. RUNTIME.osType .. ".txt"
+    local fetch_url = "https://fastly.jsdelivr.net/gh/yeshan333/vfox-mongo@main/assets/" .. RUNTIME.osType .. "_versions.txt"
     local resp, err = http.get({
         url = fetch_url
     })
